f55dce2be4d4d2e005f18b7fd68a503fae2da5ff
Deep-Mhatre <mhatredeep27@gmail.com>
2026-03-12 19:05:07 +0530

    Add initial variables and print statement in variables.py

---
+++ programming_foundations/python_fundamentals/variables.py
@@ -0,0 +1,7 @@
+name="deep"
+age=21
+print("My name is", name, "and I am", age, "years old.")
+
+# indentation is important in python 
+# identifiers are case-sensitive
+# variable names should be descriptive and cannot start with a number or contain spaces
