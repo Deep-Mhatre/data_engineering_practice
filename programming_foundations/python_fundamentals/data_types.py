65afab511183f8de8c791438889951edc5a9636c
Deep-Mhatre <mhatredeep27@gmail.com>
2026-03-13 17:47:40 +0530

    Add initial Python files for data types and operators examples

---
+++ programming_foundations/python_fundamentals/data_types.py
@@ -0,0 +1,49 @@
+'''
+name="deep"
+age=21
+pi=3.14
+
+#integer = +ve or -ve whole numbers without decimal points
+print(type(age))
+
+#string = sequence of characters enclosed in single or double quotes
+print(type(name))
+
+#float = real numbers with decimal points
+print(type(pi))
+
+#boolean = True or False
+is_student=True
+print(type(is_student))
+
+#none = absence of a value
+print(type(None))
+
+
+#keywords = reserved words in python that cannot be used as variable names
+# false, true, none, and, or, not, if, else, elif, while, for, break, cont
+# inue, pass, def, return, class, try, except, finally, raise, import, from, as, global, nonlocal, assert, lambda
+# these are some of the keywords in python and there are many more. You can find the complete list of keywords in the official python documentation.
+
+#style guide = PEP 8 is the official style guide for python code. It provides guidelines for writing clean and readable code. Some of the key points from PEP 8 include:
+# use 4 spaces for indentation
+# use lowercase letters for variable names and separate words with underscores
+# use descriptive variable names
+#snake_case = variable_name
+tot_price=100
+print(type(tot_price))
+#camelCase = variableName
+#PascalCase = VariableName
+'''
+
+#WAP to calculate sum of two numbers and print the result.
+num1=10
+num2=20
+sum = num1 + num2
+diff= num2 - num1
+mul = num1 * num2
+div = num1 / num2
+print("sum is equal to", sum)
+print("differnce is equal to", diff)
+print("multplication is equal to", mul)
+print("division is equal to", div)
---
+++ programming_foundations/python_fundamentals/operators.py
@@ -0,0 +1,38 @@
+'''
+#Arithmetic - ( + - * / % **)
+a=4 
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)
+print(a%b) #modulo
+print(a**b) #power
+'''
+
+'''
+#relational/Comparison - (>, >=, <, <=, ==, !=)
+a=6
+b=3
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+'''
+
+'''
+#Assingment (=, +=, -=, /=, %=, **=)
+a=5
+a += 3
+print(a)
+'''
+
+#logical (not, and, or)
+var = False
+print (not (10>7))
+print((6>9) and (5<8)) #True
+print((9>6) or (5<10))
+
